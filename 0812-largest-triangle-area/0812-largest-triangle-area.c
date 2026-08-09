68acde95b2037f4da64c9296daf3927cc2bf397b
MD_Ajaz uddin <120260451+Rigitro@users.noreply.github.com>
2026-08-09 09:41:03 +0530

    Time: 0 ms (100%), Space: 9.5 MB (25.18%) - LeetHub

---
+++ 0812-largest-triangle-area/0812-largest-triangle-area.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+double largestTriangleArea(int** points, int pointsSize, int* pointsColSize) {
+    double maxArea = 0.0;
+    
+
+    for (int i = 0; i < pointsSize - 2; i++) {
+        for (int j = i + 1; j < pointsSize - 1; j++) {
+            for (int k = j + 1; k < pointsSize; k++) {
+                
+                double x1 = points[i][0], y1 = points[i][1];
+                double x2 = points[j][0], y2 = points[j][1];
+                double x3 = points[k][0], y3 = points[k][1];
+                
+
+                double currentArea = 0.5 * fabs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+                
+
+                if (currentArea > maxArea) {
+                    maxArea = currentArea;
+                }
+            }
+        }
+    }
+    
+    return maxArea;
+}
