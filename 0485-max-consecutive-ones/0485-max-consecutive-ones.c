13b1b84000ea740cf2dbd6e968de299b8b9f4621
MD_Ajaz uddin <120260451+Rigitro@users.noreply.github.com>
2026-09-14 13:24:04 +0530

    Time: 0 ms (100%), Space: 13.3 MB (26.18%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.c
@@ -0,0 +1,19 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int count = 0;
+    int max = 0;
+    for(int i = 0;i<numsSize;i++){
+        if(nums[i] == 1){
+            count++;
+        }
+        if(nums[i] ==0){
+            if(count > max){
+                max = count;
+            }
+            count =0;
+        }
+    }
+    if(count > max){
+        max = count;
+    }
+    return max;
+}
